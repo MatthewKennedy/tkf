960559bf9708f518f1c2cb37d494eb413a12e509
Matthew Kennedy <m.kennedy@me.com>
2025-09-22 11:22:51 +0100

    Fix up registrations

--- app/controllers/spree/user_registrations_controller.rb
+++ app/controllers/spree/user_registrations_controller.rb
@@ -8,6 +8,31 @@ module Spree
       super
     end
 
+    # POST /resource
+      def create
+        build_resource(sign_up_params)
+
+        verify_recaptcha(action: 'signup', minimum_score: 0.5) && resource.save
+
+        yield resource if block_given?
+        if resource.persisted?
+          if resource.active_for_authentication?
+            set_flash_message! :notice, :signed_up
+            sign_up(resource_name, resource)
+            respond_with resource, location: after_sign_up_path_for(resource)
+          else
+            set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+            expire_data_after_sign_in!
+            respond_with resource, location: after_inactive_sign_up_path_for(resource)
+          end
+        else
+          clean_up_passwords resource
+          set_minimum_password_length
+          set_flash_message! :notice, :failed_attempt
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
+      end
+
     protected
 
     def translation_scope
