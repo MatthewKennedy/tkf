5c1289cdd5bb8bf68b2d7255df1236d30ef6e6ec
Matthew Kennedy <m.kennedy@me.com>
2025-09-22 09:23:46 +0100

    lint

--- config/initializers/new_google_recaptcha.rb
+++ config/initializers/new_google_recaptcha.rb
@@ -1,9 +1,9 @@
 Recaptcha.configure do |config|
-  config.site_key   = Rails.application.credentials.dig(:recaptcha, :site_key)
-  config.secret_key = Rails.application.credentials.dig(:recaptcha, :secret_key)
+  config.site_key   = Rails.application.credentials.dig(:recaptcha, :site_key) || ENV["SITE_KEY"]
+  config.secret_key = Rails.application.credentials.dig(:recaptcha, :secret_key) || ENV["SECRET_KEY"]
 
   # Uncomment the following lines if you are using the Enterprise API:
   config.enterprise            = true
-  config.enterprise_api_key    = Rails.application.credentials.dig(:recaptcha, :enterprise_api_key)
-  config.enterprise_project_id = Rails.application.credentials.dig(:recaptcha, :enterprise_project_id)
+  config.enterprise_api_key    = Rails.application.credentials.dig(:recaptcha, :enterprise_api_key) || ENV["ENTERPRISE_API_KEY"]
+  config.enterprise_project_id = Rails.application.credentials.dig(:recaptcha, :enterprise_project_id) || ENV["ENTERPRISE_PROJECT_ID"]
 end
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -82,7 +82,7 @@ Rails.application.config.after_initialize do
   # Rails.application.config.spree.page_sections << Spree::PageSections::ContactFormToGetInTouch
   # Rails.application.config.spree.page_blocks << Spree::PageBlocks::BigRedButtonToCallSales
 
-  Rails.application.config.spree_storefront.head_partials << 'spree/shared/custom_head'
+  Rails.application.config.spree_storefront.head_partials << "spree/shared/custom_head"
 end
 
 Spree.user_class = "Spree::User"
