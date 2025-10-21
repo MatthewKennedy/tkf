55ee73ef83cf67fa9baaca353ded58033499bfed
Matthew Kennedy <m.kennedy@me.com>
2025-10-21 22:38:12 +0100

    Add blob

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 
     policy.default_src :self, :https, cdn_host
     policy.font_src    :self, :https, :data, cdn_host
-    policy.img_src     :self, :https, :data, cdn_host
+    policy.img_src     :self, :https, :data, :blob, cdn_host
     policy.object_src  :none
     policy.script_src  :self, :https, :unsafe_inline, :blob, cdn_host
     policy.worker_src  :self, :blob
@@ -12,6 +12,6 @@ Rails.application.configure do
     policy.connect_src :self, :https, cdn_host
   end
 
-  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_generator  = ->(request) { request.session.id.to_s }
   config.content_security_policy_nonce_directives = %w[unsafe-inline]
 end
