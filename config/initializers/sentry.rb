a49bab21a34a9bf3eae95a54322bfd760d4d2d32
Matthew Kennedy <m.kennedy@me.com>
2026-01-23 19:30:10 +0000

    Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,31 +1,34 @@
 # frozen_string_literal: true
 
-require "sentry/transport/http_transport"
-
-module Sentry
-  class CloudflareTransport < HTTPTransport
-    def send_data(data)
-      @headers ||= {}
-
-      client_id = ::Rails.application.credentials.dig(:cf_access_client_id)
-      client_secret = ::Rails.application.credentials.dig(:cf_access_client_secret)
-
-      if client_id && client_secret
-        @headers["CF-Access-Client-Id"] = client_id.to_s
-        @headers["CF-Access-Client-Secret"] = client_secret.to_s
-      end
-
-      super(data)
-    end
-  end
-end
-
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:sentry_dsn)
+
+  # Only send events in these environments
+  config.enabled_environments = %w[production staging]
   config.environment = Rails.env
-  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
-  config.send_default_pii = true
-  config.traces_sample_rate = 1.0
-  config.excluded_exceptions += [ "SystemExit" ]
-  config.transport.transport_class = Sentry::CloudflareTransport
-end unless Rails.env.development?
+
+  # Breadcrumbs: The "trail" leading to the error
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger, :sentry_logger ]
+
+  # Performance & Profiling
+  # 1.0 is high for production; 0.1 (10%) is usually plenty.
+  config.traces_sample_rate = 0.1
+  config.profiles_sample_rate = 0.1
+
+  # Data Privacy
+  config.send_default_pii = true # Be careful: this sends user IPs and cookies.
+
+  # Filtering
+  config.excluded_exceptions += [ "SystemExit", "SignalException" ]
+
+  # Use Rails' backtrace cleaner to make logs readable
+  config.backtrace_cleanup_callback = lambda do |backtrace|
+    Rails.backtrace_cleaner.clean(backtrace)
+  end
+
+  config.enable_logs = true
+  config.enabled_patches << :logger
+  config.std_lib_logger_filter = proc do |logger, message, severity|
+    [ :error, :fatal ].include?(severity)
+  end
+end
