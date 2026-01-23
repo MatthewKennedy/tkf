0195438b67065cc806385674c3a10f0bbdacd6f1
Matthew Kennedy <m.kennedy@me.com>
2026-01-23 18:24:32 +0000

    Set up

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,8 +5,8 @@ require "sentry/transport/http_transport"
 module Sentry
   class CloudflareTransport < HTTPTransport
     def send_data(data)
-      client_id     = Rails.application.credentials.dig(:cf_access_client_id)
-      client_secret = Rails.application.credentials.dig(:cf_access_client_secret)
+      client_id     = ::Rails.application.credentials.dig(:cf_access_client_id)
+      client_secret = ::Rails.application.credentials.dig(:cf_access_client_secret)
 
       if client_id && client_secret
         @headers["CF-Access-Client-Id"] = client_id
