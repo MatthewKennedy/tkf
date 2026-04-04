1fa8574b005c942b3b7ceb3050ce9dab31dcff5f
Matthew Kennedy <m.kennedy@me.com>
2026-04-04 21:42:52 +0100

    set active storage proxy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,5 @@ Rails.application.configure do
   config.host_authorization = { exclude: ->(request) { request.path == "/healthz" } }
 end
 
+Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
 Rails.application.routes.default_url_options[:host] = "tongkat.fitness"
